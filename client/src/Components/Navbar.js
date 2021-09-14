055e6b1d59c0224fcf7ad977b0974d9b8cd865df
Dhruvil Shah <dhruvilshah2001@gmail.com>
2021-09-14 21:57:32 +0530

    Navbar Login Signup Component Created

---
+++ client/src/Components/Navbar.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { Home } from './Home';
+import { Login } from './Login.js';
+import { SignUp } from './SignUp';
+import '../CSS/navbar.css'
+export const Navbar = () => {
+    return (
+        <div>
+            <Router>
+        <div className='nav'>
+          <h2 className='title'>Movies Recommendation System</h2>
+          <ul>
+            <li className='links'><Link className='inside' to={'/'} > Home </Link></li>
+            <li className='links'><Link className='inside' to={'/login'} >Login</Link></li>
+            <li className='links'><Link className='inside' to={'/signup'} >SignUp</Link></li>
+          </ul>
+          </div>
+          <div>          <Switch>
+              <Route exact path='/' component={Home} />
+              <Route path='/login' component={Login} />
+              <Route path='/signup' component={SignUp} />
+          </Switch>
+        </div>
+      </Router>
+        </div>
+    )
+}
