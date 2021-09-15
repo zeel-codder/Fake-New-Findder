5049bd3029a93d4d7faf7bc1552426c3bf8f9f4d
Dhruvil Shah <dhruvilshah2001@gmail.com>
2021-09-15 12:36:49 +0530

    Hooks added in form

--- client/src/Components/Login.js
+++ client/src/Components/Login.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import '../CSS/login.css';
+import { useState } from 'react';
 export const Login = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const Verify=(e)=>{
+        e.preventDefault();
+        console.log(email+" "+password);
+        // User Authentication through Firebase
+    }
     return (
         <>
         <div className="container">
             <form action="POST">
-               <input type="email" placeholder='Enter Email Here'></input> <br/>
-               <input type="password" placeholder='Enter Password Here'></input><br/>
-               <button>Login</button>
+               <input type="email" placeholder='Enter Email Here' value={email} onChange={(e)=>{setEmail(e.target.value)}}></input> <br/>
+               <input type="password" placeholder='Enter Password Here' value={password} onChange={(e)=>{setPassword(e.target.value)}}></input><br/>
+               <button onClick={Verify}>Login</button>
             </form>
         </div>
         </>
--- client/src/Components/SignUp.js
+++ client/src/Components/SignUp.js
@@ -1,15 +1,24 @@
 import React from 'react'
 import '../CSS/signup.css';
+import { useState } from 'react';
 export const SignUp = () => {
+    const [email, setEmail] = useState("");
+    const [phone, setPhone] = useState("");
+    const [password, setPassword] = useState("");
+    const [repassword, setRepassword] = useState("");
+    const Verify=(e)=>{
+        e.preventDefault();
+        console.log(email+" "+phone);
+    }
     return (
         <>
             <div className="container">
             <form action="POST">
-               <input type="email" placeholder='Enter Email Here'></input> <br/>
-               <input type="text" placeholder='Enter Phone Number Here'></input> <br/>
-               <input type="password" placeholder='Enter Password Here'></input><br/>
-               <input type="password" placeholder='Re-Enter Password Here'></input><br/>
-               <button>Sign Up</button>
+               <input type="email" placeholder='Enter Email Here' onChange={(e)=>{setEmail(e.target.value)}}></input> <br/>
+               <input type="text" placeholder='Enter Phone Number Here' onChange={(e)=>{setPhone(e.target.value)}}></input> <br/>
+               <input type="password" placeholder='Enter Password Here' onChange={(e)=>{setPassword(e.target.value)}}></input><br/>
+               <input type="password" placeholder='Re-Enter Password Here' onChange={(e)=>{setRepassword(e.target.value)}}></input><br/>
+               <button onClick={Verify}>Sign Up</button>
             </form>
         </div>
         </>
