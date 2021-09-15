cc0087a573e07a6de20e7c63280035dc167f524e
Dhruvil Shah <dhruvilshah2001@gmail.com>
2021-09-15 11:49:36 +0530

    Login SignUp UI done

--- client/src/Components/Login.js
+++ client/src/Components/Login.js
@@ -1,9 +1,15 @@
 import React from 'react';
-
+import '../CSS/login.css';
 export const Login = () => {
     return (
-        <div>
-            Login Page
+        <>
+        <div className="container">
+            <form action="POST">
+               <input type="email" placeholder='Enter Email Here'></input> <br/>
+               <input type="password" placeholder='Enter Password Here'></input><br/>
+               <button>Login</button>
+            </form>
         </div>
+        </>
     )
 }
--- client/src/Components/SignUp.js
+++ client/src/Components/SignUp.js
@@ -1,9 +1,17 @@
 import React from 'react'
-
+import '../CSS/signup.css';
 export const SignUp = () => {
     return (
-        <div>
-            SignUp page
+        <>
+            <div className="container">
+            <form action="POST">
+               <input type="email" placeholder='Enter Email Here'></input> <br/>
+               <input type="text" placeholder='Enter Phone Number Here'></input> <br/>
+               <input type="password" placeholder='Enter Password Here'></input><br/>
+               <input type="password" placeholder='Re-Enter Password Here'></input><br/>
+               <button>Sign Up</button>
+            </form>
         </div>
+        </>
     )
 }
